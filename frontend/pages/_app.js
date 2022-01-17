717219bb3f7d88ba64242ca10362c9381d1be0f9
Erdem <erdem.ozg@gmail.com>
2022-01-17 22:01:47 +0300

    added title

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -19,6 +19,7 @@ function MyApp({ Component, pageProps }) {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet" />
+          <title>Audiominer</title>
         </Head>
         <Component {...pageProps} />
       </AppWrapper>
